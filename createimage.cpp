b058e54aa1b785f35b4e80cef47e479b1eee9622
Ethan Duckmanton <ecd@Ethans-MacBook-Air-3.local>
2025-06-20 16:47:56 +0100

    added map

--- createimage.cpp
+++ createimage.cpp
@@ -1,3 +1,4 @@
+#include<cmath>
 #include<iostream>
 #include<fstream>
 #include<vector>
@@ -32,15 +33,57 @@ void gen_ppm_image(const std::string filename, const std::vector<uint32_t> &fb, 
   ofs.close();
 }
 
+void draw_rectangle(std::vector<uint32_t> &image, const size_t imgW, const size_t imgH, const size_t x, const size_t y, const size_t W, const size_t H, const uint32_t color)
+{
+  assert(image.size() == imgW*imgH);
+  // iterate over the dimensions of the rectangle, off set all points by the x and y
+  for(size_t i = 0; i < W; i++)
+  {
+    for(size_t j = 0; j < H; j++)
+    {
+      // coords of point in rectangle in frame of image
+      // x+i, y+j
+      size_t cx = x+i;
+      size_t cy = y+j;
+      assert(cx < imgW && cy < imgH); // check point is in bounds
+      image[cx + cy*imgW] = color;
+    }
+  }
+}
+
 int main() 
 {
+// constants
   const size_t winH = 512;
   const size_t winW = 512; // image dimensions
-
   std::vector<uint32_t> framebuffer(winW * winH, 255);
   // define as a linear vector of colors
   // we are storing the color of each pixel as a uinit32_t, a 4 byte integer with each byte
   // representing rgb and a. This is a vector of size width*height, initialised to red (255)
+  
+  const size_t mapH = 16;
+  const size_t mapW = 16;
+  const char map[] = "0000222222220000"\
+                    "1              0"\
+                    "1      11111   0"\
+                    "1     0        0"\
+                    "0     0  1110000"\
+                    "0     3        0"\
+                    "0   10000      0"\
+                    "0   0   11100  0"\
+                    "0   0   0      0"\
+                    "0   0   1  00000"\
+                    "0       1      0"\
+                    "2       1      0"\
+                    "0       0      0"\
+                    "0 0000000      0"\
+                    "0              0"\
+                    "0002222222200000"; // this map is taken from the tutorial I cba
+            
+  // game map is saved as a char array, so spaces will be uneffected
+  assert(sizeof(map) == mapH*mapW + 1); // +1 for the string termination character
+                    
+  // this generates the image without the map
   for (size_t irow = 0; irow<winH; irow++) // iterate over all rows
   {
     for (size_t ipixel = 0; ipixel<winW; ipixel++)
@@ -53,7 +96,33 @@ int main()
     }
   }
 
+  // this adds on the map by drawing rectangle on top of the existing fb
+  // for each non space point on the map
+  const size_t rectW = winW/mapW;
+  const size_t rectH = winH/mapH;
+  for(int j = 0; j < mapH; j++)
+  {
+    for(int i = 0; i < mapW; i++)
+    {
+      if(map[i+(j*mapW)] == ' ')
+      {
+        std::cout<<'e';
+        continue; // skip spaces
+      }
+      std::cout<<'n';
+      size_t rect_y = j*rectH;
+      size_t rect_x = i*rectW;
+      draw_rectangle(framebuffer, winW, winH, rect_x, rect_y, rectW, rectH, pack_color(0,255,255));
+    }
+  }
+
+  // draw the plater, coords in map segments
+  float player_x = 3;
+  float player_y = 2;
+
+  // generates the image from the framebuffer
   gen_ppm_image("./out.ppm", framebuffer, winW, winH);
+
   std::cout<<"EXITING"<<std::endl;
   return 0;
 }
